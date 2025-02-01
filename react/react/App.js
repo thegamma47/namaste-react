6e22925aab46d475979b1cc22ffcc1a4253861d3
abhay rajpoot <abhayrajput11104@gmail.com>
2025-02-01 23:51:56 +0530

    Initial commit

---
+++ react/react/App.js
@@ -0,0 +1,8 @@
+const heading = react.createElement(
+    "h1",
+    {Id:"heading",xyz:"abc"},
+    "Hello World from react!");
+
+const root = react-dom.createRoot(document.getElementbyId("root"));
+
+root.render(heading);
