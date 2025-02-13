8b8078807111448237355a0319de74f0a0617ec8
thegamma47 <127945659+thegamma47@users.noreply.github.com>
2025-02-14 01:40:11 +0530

    Delete react/react directory

--- react/react/App.js
+++
@@ -1,8 +0,0 @@
-const heading = react.createElement(
-    "h1",
-    {Id:"heading",xyz:"abc"},
-    "Hello World from react!");
-
-const root = react-dom.createRoot(document.getElementbyId("root"));
-
-root.render(heading);
